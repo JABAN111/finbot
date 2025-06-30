aae403d7aabc957451ade6df3ceedeb56c4f7318
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-07-01 00:59:36 +0300

    add commands

--- telegram.go
+++ telegram.go
@@ -23,11 +23,18 @@ func NewTelegramManager(bot *tgbotapi.BotAPI, offset, timeout, numWorkers int, d
 	tgConfig := tgbotapi.NewUpdate(offset)
 	tgConfig.Timeout = timeout
 
-	return TelegramManager{
+	tm := TelegramManager{
 		bot:        bot,
 		log:        GetLogger(),
 		numWorkers: numWorkers,
 	}
+
+	err := tm.setCommands()
+	if err != nil {
+		log.Error("fail to set commands for telegram bot", "error", err)
+		panic(fmt.Errorf("fail to set commands for telegram bot: %e", err))
+	}
+	return tm
 }
 
 func (tm *TelegramManager) ListenAndServe(ctx context.Context) error {
@@ -48,10 +55,20 @@ func (tm *TelegramManager) ListenAndServe(ctx context.Context) error {
 					if !ok {
 						return
 					}
+					fmt.Println("Received update:", update)
 
-					if update.CallbackQuery != nil {
-						chatID := update.CallbackQuery.ID
-						fmt.Println("Received update from chat ID:", chatID)
+					if msg := update.Message; msg != nil {
+						switch msg.Command() {
+						case "start":
+							tm.log.Info("Received /start command", "user_id", msg.From.ID)
+							// Handle /start command
+
+						case "help":
+							tm.log.Info("Received /help command", "user_id", msg.From.ID)
+
+						case "reset":
+							tm.log.Info("Received /reset command", "user_id", msg.From.ID)
+						}
 					}
 
 				}
@@ -61,4 +78,20 @@ func (tm *TelegramManager) ListenAndServe(ctx context.Context) error {
 
 	wg.Wait()
 	return nil
+}
+
+func (tm *TelegramManager) setCommands() error {
+	cfg := tgbotapi.NewSetMyCommands(tgbotapi.BotCommand{
+		Command:     "start",
+		Description: "Начать работу с ботом",
+	}, tgbotapi.BotCommand{
+		Command:     "help",
+		Description: "Запросить описание взаимодействия с ботом",
+	}, tgbotapi.BotCommand{
+		Command:     "reset",
+		Description: "Сбрасывает состояние заполнение данных для операции",
+	})
+
+	_, err := tm.bot.Request(cfg)
+	return err
 }
