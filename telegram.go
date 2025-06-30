0655dde7333821677e542324dfd09e741770eccc
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-07-01 01:55:39 +0300

    start working with callbacks

--- telegram.go
+++ telegram.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	errNotCommand = errors.New("this msg is not a command")
+	errUnknownCommand = errors.New("get unknown command")
 )
 
 const (
@@ -26,6 +26,7 @@ type TelegramManager struct {
 	bot          *tgbotapi.BotAPI
 	updateConfig tgbotapi.UpdateConfig
 	log          *slog.Logger
+	mu           sync.RWMutex
 }
 
 func NewTelegramManager(bot *tgbotapi.BotAPI, offset, timeout, numWorkers int, debug bool) TelegramManager {
@@ -68,15 +69,17 @@ func (tm *TelegramManager) ListenAndServe(ctx context.Context) error {
 						return
 					}
 
+					tm.mu.Lock()
 					currentUserState, exist := users[update.Message.From.ID]
 					if !exist {
 						currentUserState = UserState{}
 						users[update.Message.From.ID] = currentUserState
 					}
+					tm.mu.Unlock()
 
 					if update.Message != nil {
 						if err := tm.processMessage(update); err != nil {
-							if errors.Is(err, errNotCommand) {
+							if errors.Is(err, errUnknownCommand) {
 								continue
 							}
 							tm.log.Error("fail to process command", "error", err, "user_id", update.Message.From.ID)
@@ -110,9 +113,21 @@ func (tm *TelegramManager) setCommands() error {
 
 func (tm *TelegramManager) processMessage(update tgbotapi.Update) error {
 	msg := update.Message
+
+	if update.CallbackQuery != nil {
+		return tm.handleCallback(update)
+	}
+
 	switch msg.Command() {
 	case commandStart:
 		tm.log.Info("Received /start command", "user_id", msg.From.ID)
+		keyboard := tm.createMainInlineCommands()
+		responseMsg := tgbotapi.NewMessage(msg.Chat.ID, "Выберите действие")
+		responseMsg.ReplyMarkup = keyboard
+		_, err := tm.bot.Send(responseMsg)
+		if err != nil {
+			return err
+		}
 		return nil
 	case commandHelp:
 		tm.log.Info("Received /help command", "user_id", msg.From.ID)
@@ -123,16 +138,65 @@ func (tm *TelegramManager) processMessage(update tgbotapi.Update) error {
 		}
 
 	case commandReset:
+		tm.mu.Lock()
 		tm.log.Info("Received /reset command", "user_id", msg.From.ID)
 		users[update.Message.Chat.ID] = UserState{}
 		responseMsg := tgbotapi.NewMessage(msg.Chat.ID, "Статус заполнения данных для операции сброшен")
 		if _, err := tm.bot.Send(responseMsg); err != nil {
 			return err
 		}
+		tm.mu.Unlock()
 
 	default:
 		tm.log.Warn("Received unknown command", "command", msg.Command(), "user_id", msg.From.ID)
-		return errNotCommand
+		responseMsg := tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("Неизвестная команда: %s", msg.Command()))
+		if _, err := tm.bot.Send(responseMsg); err != nil {
+			return err
+		}
+		return errUnknownCommand
 	}
+
+	return nil
+}
+
+func (tm *TelegramManager) createMainInlineCommands() tgbotapi.InlineKeyboardMarkup {
+	return tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Пополнение", "refill"),
+			tgbotapi.NewInlineKeyboardButtonData("Снятие", "remove"),
+		),
+	)
+}
+
+func (tm *TelegramManager) createChooseCategoryInlineCommands() tgbotapi.InlineKeyboardMarkup {
+	return tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Продукты", "category_products"),
+			tgbotapi.NewInlineKeyboardButtonData("Транспорт", "category_transport"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Развлечения", "category_entertainment"),
+			tgbotapi.NewInlineKeyboardButtonData("Коммуналка", "category_utility"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Назад", "back_to_main"),
+		),
+	)
+}
+
+func (tm *TelegramManager) handleCallback(update tgbotapi.Update) error {
+	msg := update.Message
+	//chatID := msg.Chat.ID
+	userID := msg.From.ID
+
+	tm.mu.Lock()
+	currentUserState, ok := users[userID]
+	if !ok {
+		tm.log.Error("user missed in users map", "userID", userID)
+		currentUserState = UserState{}
+		users[userID] = currentUserState
+	}
+	tm.mu.Unlock()
+
 	return nil
 }
