e8978b51868fe65d556ce7107cb53c676edb05c4
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-06-30 16:03:21 +0300

    add test for notion insert to db operation

--- notion_test.go
+++ notion_test.go
@@ -4,51 +4,10 @@ import (
 	"context"
 	"github.com/dstotijn/go-notion"
 	"github.com/joho/godotenv"
-	"log/slog"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
 )
-
-func TestNotionManager_PrintPage(t *testing.T) {
-	type fields struct {
-		log    *slog.Logger
-		client *notion.Client
-	}
-	type args struct {
-		ctx    context.Context
-		pageID string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "attempt to get a page",
-			fields: fields{
-				log:    GetLogger(),
-				client: getNotionClient(),
-			},
-			args: args{
-				ctx:    context.Background(),
-				pageID: "21d3d8e126bf8096ae11f9a4d100b714",
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			n := &NotionManager{
-				log:    tt.fields.log,
-				client: tt.fields.client,
-			}
-			if err := n.PrintPage(tt.args.ctx, tt.args.pageID); (err != nil) != tt.wantErr {
-				t.Errorf("PrintPage() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
 
 func getNotionClient() *notion.Client {
 	err := godotenv.Load()
@@ -57,22 +16,43 @@ func getNotionClient() *notion.Client {
 	}
 
 	token := os.Getenv("NOTION_SECRET")
-
+	if len(token) == 0 {
+		panic("NOTION_SECRET is not set")
+	}
 	return notion.NewClient(token)
 }
 
-func TestNotionManager_QueryDatabase(t *testing.T) {
-	c := getNotionClient()
-	nm := NewNotionManager(c)
+func TestNotionManager_InsertOperation(t *testing.T) {
+	err := godotenv.Load()
+	require.NoError(t, err)
+	databaseID := os.Getenv("NOTION_DATABASE_ID")
 
-	nm.QueryDatabase()
-}
+	ctx := context.Background()
 
-func TestNotionManager_InsertToDb(t *testing.T) {
-
-	c := getNotionClient()
-	nm := NewNotionManager(c)
-
-	nm.InsertToDb(context.Background())
-
+	tests := []struct {
+		name    string
+		dto     InsertOperationDto
+		wantErr bool
+	}{
+		{
+			name: "fully valid insert",
+			dto: InsertOperationDto{
+				Creator:  "jaba368",
+				Category: "продукты",
+				Sum:      38218321,
+				Status:   OperationStatusRefill,
+				Comment:  "Обычное добавление после теста",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			notionClient := getNotionClient()
+			nm := NewNotionManager(notionClient)
+			if err := nm.InsertOperation(ctx, databaseID, tt.dto); (err != nil) != tt.wantErr {
+				t.Errorf("InsertOperation() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
