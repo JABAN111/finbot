49e7fb759b424bcf3add0135ec0811b8a2ed21af
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-07-01 01:18:32 +0300

    add logic for processing commands

--- telegram.go
+++ telegram.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log/slog"
 	"sync"
+)
+
+var (
+	errNotCommand = errors.New("this msg is not a command")
+)
+
+const (
+	commandStart = "start"
+	commandHelp  = "help"
+	commandReset = "reset"
 )
 
 var users = make(map[int64]UserState)
@@ -53,21 +64,22 @@ func (tm *TelegramManager) ListenAndServe(ctx context.Context) error {
 					return
 				case update, ok := <-upChan:
 					if !ok {
+						tm.log.Error("updates channel is closed")
 						return
 					}
-					fmt.Println("Received update:", update)
 
-					if msg := update.Message; msg != nil {
-						switch msg.Command() {
-						case "start":
-							tm.log.Info("Received /start command", "user_id", msg.From.ID)
-							// Handle /start command
+					currentUserState, exist := users[update.Message.From.ID]
+					if !exist {
+						currentUserState = UserState{}
+						users[update.Message.From.ID] = currentUserState
+					}
 
-						case "help":
-							tm.log.Info("Received /help command", "user_id", msg.From.ID)
-
-						case "reset":
-							tm.log.Info("Received /reset command", "user_id", msg.From.ID)
+					if update.Message != nil {
+						if err := tm.processMessage(update); err != nil {
+							if errors.Is(err, errNotCommand) {
+								continue
+							}
+							tm.log.Error("fail to process command", "error", err, "user_id", update.Message.From.ID)
 						}
 					}
 
@@ -82,16 +94,45 @@ func (tm *TelegramManager) ListenAndServe(ctx context.Context) error {
 
 func (tm *TelegramManager) setCommands() error {
 	cfg := tgbotapi.NewSetMyCommands(tgbotapi.BotCommand{
-		Command:     "start",
+		Command:     commandStart,
 		Description: "Начать работу с ботом",
 	}, tgbotapi.BotCommand{
-		Command:     "help",
+		Command:     commandHelp,
 		Description: "Запросить описание взаимодействия с ботом",
 	}, tgbotapi.BotCommand{
-		Command:     "reset",
+		Command:     commandReset,
 		Description: "Сбрасывает состояние заполнение данных для операции",
 	})
 
 	_, err := tm.bot.Request(cfg)
 	return err
+}
+
+func (tm *TelegramManager) processMessage(update tgbotapi.Update) error {
+	msg := update.Message
+	switch msg.Command() {
+	case commandStart:
+		tm.log.Info("Received /start command", "user_id", msg.From.ID)
+		return nil
+	case commandHelp:
+		tm.log.Info("Received /help command", "user_id", msg.From.ID)
+		helpText := "Здесь будет текст для команды /help"
+		responseMsg := tgbotapi.NewMessage(msg.Chat.ID, helpText)
+		if _, err := tm.bot.Send(responseMsg); err != nil {
+			return err
+		}
+
+	case commandReset:
+		tm.log.Info("Received /reset command", "user_id", msg.From.ID)
+		users[update.Message.Chat.ID] = UserState{}
+		responseMsg := tgbotapi.NewMessage(msg.Chat.ID, "Статус заполнения данных для операции сброшен")
+		if _, err := tm.bot.Send(responseMsg); err != nil {
+			return err
+		}
+
+	default:
+		tm.log.Warn("Received unknown command", "command", msg.Command(), "user_id", msg.From.ID)
+		return errNotCommand
+	}
+	return nil
 }
