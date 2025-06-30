145257c39c13e7ed2ecc9902c01f92bb5fe38ca7
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-06-30 15:47:36 +0300

    delete methods for debugging

--- notion.go
+++ notion.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/dstotijn/go-notion"
 	"log/slog"
 	"time"
@@ -17,24 +16,12 @@ const (
 )
 
 const (
-	ColorGray   = Color("Gray")
-	ColorBrown  = Color("Brown")
-	ColorOrange = Color("Orange")
-	ColorYellow = Color("Yellow")
-	ColorGreen  = Color("Green")
-	ColorBlue   = Color("Blue")
-	ColorPurple = Color("Purple")
-	ColorPink   = Color("Pink")
-	ColorRed    = Color("Red")
-)
-
-const (
-	ColumnWho           = "Кто"
-	ColumnOperationSum  = "Сумма операции"
-	ColumnComment       = "Комментарий"
-	ColumnCategory      = "Категория"
-	ColumnStatus        = "Статус"
-	ColumnOperationDate = "Дата внесения"
+	columnWho           = "Кто"
+	columnOperationSum  = "Сумма операции"
+	columnComment       = "Комментарий"
+	columnCategory      = "Категория"
+	columnStatus        = "Статус"
+	columnOperationDate = "Дата внесения"
 )
 
 type NotionManager struct {
@@ -55,97 +42,53 @@ type InsertOperationDto struct {
 	Sum      float64
 	Status   OperationStatus
 	Comment  string
-	Color    Color
 }
 
-func (n *NotionManager) PrintPage(ctx context.Context, pageID string) error {
-	page, err := n.client.FindPageByID(ctx, pageID)
-	if err != nil {
-		n.log.Error("fail to find a page by id", "err", err, "page id", pageID)
-		return err
-	}
-
-	fmt.Printf("found page: %v\n", page)
-	return nil
-}
-
-func (n *NotionManager) QueryDatabase() {
-	databaseID := "21d3d8e126bf8022a26dc42a29697985"
-	ctx := context.Background()
-
-	query := &notion.DatabaseQuery{}
-	resp, err := n.client.QueryDatabase(ctx, databaseID, query)
-	if err != nil {
-		n.log.Error("ошибка при запросе базы данных", "err", err)
-		panic(err)
-	}
-	for _, p := range resp.Results {
-		fmt.Printf("got page: %v\n", p)
-
-		switch props := p.Properties.(type) {
-		case notion.PageProperties:
-			for _, rt := range props.Title.Title {
-				fmt.Println("Заголовок:", rt.Text.Content)
-			}
-		case notion.DatabasePageProperties:
-			for name, prop := range props {
-				fmt.Println("name", name)
-				value := prop.Value()
-				fmt.Println(value)
-			}
-		default:
-			log.Error(fmt.Sprintf("неожиданный тип Properties: %T", props))
-			panic(err)
-		}
-
-	}
-}
-
-func (n *NotionManager) InsertToDb(ctx context.Context) {
-	databaseID := "21d3d8e126bf8022a26dc42a29697985"
-
+func (n NotionManager) InsertOperation(ctx context.Context, databaseID string, operationDTO InsertOperationDto) error {
 	props := notion.DatabasePageProperties{
-		ColumnWho: notion.DatabasePageProperty{
+		columnWho: notion.DatabasePageProperty{
 			Select: &notion.SelectOptions{
-				Name: "Миша",
+				Name: operationDTO.Creator,
 			},
 		},
-		ColumnCategory: notion.DatabasePageProperty{
+		columnCategory: notion.DatabasePageProperty{
 			Select: &notion.SelectOptions{
-				Name:  "Продуктывdsamklыф",
-				Color: "pu",
+				Name: operationDTO.Category,
 			},
 		},
-		ColumnComment: notion.DatabasePageProperty{
+		columnComment: notion.DatabasePageProperty{
 			Title: []notion.RichText{
 				{
-					Text: &notion.Text{Content: "на июнь месяц"},
+					Text: &notion.Text{Content: operationDTO.Comment},
 				},
 			},
 		},
-		ColumnOperationSum: notion.DatabasePageProperty{
-			Number: notion.Float64Ptr(-32918),
+		columnOperationSum: notion.DatabasePageProperty{
+			Number: &operationDTO.Sum,
 		},
-		ColumnOperationDate: notion.DatabasePageProperty{
+		columnOperationDate: notion.DatabasePageProperty{
 			Date: &notion.Date{
 				Start: notion.NewDateTime(time.Now(), false),
 			},
 		},
-		ColumnStatus: notion.DatabasePageProperty{
+		columnStatus: notion.DatabasePageProperty{
 			Status: &notion.SelectOptions{
-				Name: "Снятие",
+				Name: string(operationDTO.Status),
 			},
 		},
 	}
+
 	params := notion.CreatePageParams{
 		ParentType:             notion.ParentTypeDatabase,
 		ParentID:               databaseID,
 		DatabasePageProperties: &props,
 	}
+
 	page, err := n.client.CreatePage(ctx, params)
 	if err != nil {
-		log.Error(fmt.Sprintf("Ошибка создания страницы: %v", err))
-		return
+		log.Error("Ошибка создания страницы", "err", err)
+		return err
 	}
-	log.Info(fmt.Sprintf("Страница создана: %s", page.URL))
+	log.Info("Страница создана", "url", page.URL)
+	return nil
 }
